4e44f503be7b385be38f35c450007068f859bfe9
Maulana Syaeikhi Naefiq <maulana.snaefiq08@gmail.com>
2024-08-31 14:49:46 +0700

    Delete cypress directory

--- cypress/support/page/home-page/home-page-locators.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    dataid: {
-        imageCompanyLogo: '//*[@id="nava"]',
-        button: (buttonType) => `//*[@type="button" and contains(text(), '${buttonType}')]`
-    }
-}
-
